0b797b5ceab1525f34de4b015f9bb0f861579fb3
Sreehari <sreekuttankzm@gmail.com>
2023-11-28 09:12:02 +0000

    Errors included

--- experiment.py
+++ experiment.py
@@ -1,36 +1,45 @@
-from experi import passwords
+import json
 import getpass
-if __name__ == '__main__':
+from saving_ps import save
+if __name__ == '__main__':    
     pass
 question = input('Login/Signup: ').lower()
 if question == "signup":
     def password_s():
-        username = input("Create username: ")
+        dict = {}   
         while True:
-            dict = {}
+            username = input("Create username: ")
+            if username in save:
+                print("Username already exists")
+                continue
             special_characters = '!@#$%&()-_[]}{;:"./<>?'
             numbers = '1234567890'
             password = getpass.getpass("Create password: ")
             try:
                 if len(password) <= 6:
-                    print('Please use a longer password')
+                    print('Password should be a minimum of 12 characters')
                     continue
                 elif any(map(lambda x: x in password, special_characters)):
                     if any(map(lambda y: y in password, numbers)):
-                        password_1 = getpass.getpass("Re-Enter password")
+                        password_1 = getpass.getpass("Re-Enter password: ")
                         if password == password_1:
-                            passwords.update({username,password_1})
-                            return
+                            dict[username] = password
+                            with open("saving_ps.py", "a") as f:
+                                json.dumps(save.update(dict),f)
+                                f.write('\n')
+                                return
                         else:
                             print('passwords do not match')
+                            continue
                     else:
                         print('Please use numeric digits')
                         continue
                 else:
                     print("Please use a stronger password")
                     continue
-            except:
-                pass
+            except Exception as e:
+                print(e)
+            break
     password_s()
 else:
     if question == "login":
