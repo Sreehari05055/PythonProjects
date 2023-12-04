db4d8ec90b160901be221e922fd440ce429b6e72
Sreehari <sreekuttankzm@gmail.com>
2023-12-04 07:36:07 +0000

    Adding

---
+++ Task3.py
@@ -0,0 +1,16 @@
+default_file = 'saving_ps.txt'
+
+dict = {}
+def read_stuff():
+    with open(default_file,'r') as f:
+        for line in f:
+            username, password = line.strip().split(' ')
+            dict[username] = password
+        return dict
+def login(passwd):
+    values_present = read_stuff()
+    return values_present[passwd]
+
+
+if __name__ == '__main__':
+    print(read_stuff())
