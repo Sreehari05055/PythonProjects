7b023a40cac839174f33855ea4a2e9247a70c56a
Sreehari <sreehari@AnuKumars-MacBook-Pro.local>
2024-02-23 11:24:54 +0000

    Adding Tasks

--- credential_gui.py
+++ credential_gui.py
@@ -68,13 +68,12 @@ def forgot_pssword():
         window_44.destroy()
     return
 def recognition():
-    global window_58
+    global window_58,newpas_entry58
     window_58 = Toplevel()
     window_58.geometry('850x500')
     new_pass58 = Label(window_58,text='Enter the code: ')
     new_pass58.config(font = ('Arial', 20))
     new_pass58.place(x=200,y=200)
-    global newpas_entry58
     newpas_entry58 = Entry(window_58, width=20, borderwidth=3,)
     newpas_entry58.place(x=350, y=200)
     subission_5 = Button(window_58, text='Submit', command=codemit).place(x=350,y=260)
@@ -92,19 +91,17 @@ def codemit():
         new_pass59.place(x=300,y=200)
     return
 def change_psswd():
-    global window_55
+    global window_55,newpas_entry,repasswd_entry
     window_55 = Toplevel()
     window_55.geometry('850x500')
     new_pass = Label(window_55,text='Enter New Password : ')
     new_pass.config(font = ('Arial', 20))
     new_pass.place(x=200,y=200)
-    global newpas_entry
     newpas_entry = Entry(window_55, width=20, borderwidth=3,)
     newpas_entry.place(x=400, y=200)
     re_passwd = Label(window_55,text='Re-Enter Password : ')
     re_passwd.config(font = ('Arial', 20))
     re_passwd.place(x=213,y=230)
-    global repasswd_entry
     repasswd_entry = Entry(window_55,show='*', width=20, borderwidth=3,)
     repasswd_entry.place(x=400, y=230)
     subission = Button(window_55, text='Submit', command=passwd_submit).place(x=390,y=260)
@@ -129,7 +126,7 @@ def passwd_submit():
                                 f.close()
                                 win_1 = Toplevel()
                                 win_1.geometry('850x500')
-                                new_pass = Label(win_1,text='Access Granted')
+                                new_pass = Label(win_1,text='Password has been changed')
                                 new_pass.config(font = ('Arial', 20))
                                 new_pass.place(x=200,y=200)
                                 window_55.destroy()
@@ -149,100 +146,94 @@ def submit_signup():
         passwd_2 = str(passwd1_entry.get()).strip()
         passwd_12 = codecs.encode(passwd_2, 'rot_13') 
         usr_2 =  str(usrname1_entry.get()).strip()
-        with open('saving_ps.txt', 'r') as f:
-            for line in f:
-                if len(line.split()) == 0:
-                    continue
-                usr, pswd, name1, gmail = line.strip().split(':')
-                dict_1[usr] = pswd, name1, gmail
-            if (usr_2) in dict_1.keys():
-                window_5 = Toplevel()
-                window_5.geometry('850x500')
-                Access_4 = Label(window_5,text='Username already exists')
-                Access_4.config(font = ('Arial', 20))
-                Access_4.place(x=300,y=200)
-            else:
-                with open('saving_ps.txt', 'a') as w:
-                    w.write('\n')
-                    w.write(usr_2)
-                    w.write(':')
-                    w.write(passwd_12)
-                    w.write(':')
-                    w.write(name_2)
-                    w.write(':')
-                    w.write(eml)
-                    w.close()
-                window_4 = Toplevel()
-                window_4.geometry('850x500')
-                Access_3 = Label(window_4,text='You have successfully created an account')
-                Access_3.config(font = ('Arial', 20))
-                Access_3.place(x=300,y=200)
-        window_1.destroy()
-        return
-
+        if '@gmail.com' not in eml:
+            submit_signup()
+        else:
+            with open('saving_ps.txt', 'r') as f:
+                for line in f:
+                    if len(line.split()) == 0:
+                        continue
+                    usr, pswd, name1, gmail = line.strip().split(':')
+                    dict_1[usr] = pswd, name1, gmail
+                if (usr_2) in dict_1.keys():
+                    window_5 = Toplevel()
+                    window_5.geometry('850x500')
+                    Access_4 = Label(window_5,text='Username already exists')
+                    Access_4.config(font = ('Arial', 20))
+                    Access_4.place(x=300,y=200)
+                else:
+                    with open('saving_ps.txt', 'a') as w:
+                        w.write('\n')
+                        w.write(usr_2)
+                        w.write(':')
+                        w.write(passwd_12)
+                        w.write(':')
+                        w.write(name_2)
+                        w.write(':')
+                        w.write(eml)
+                        w.close()
+                    window_4 = Toplevel()
+                    window_4.geometry('850x500')
+                    Access_3 = Label(window_4,text='You have successfully created an account')
+                    Access_3.config(font = ('Arial', 20))
+                    Access_3.place(x=300,y=200)
+            window_1.destroy()
+            return
 def login():
-    global window
+    global window,usrname_entry,passwd_entry
     window = Toplevel()
     window.geometry('850x500')
     usrnmae = Label(window,text='Username: ')
     usrnmae.config(font = ('Arial', 20))
     usrnmae.place(x=300,y=200)
-    global usrname_entry
     usrname_entry = Entry(window, width=20, borderwidth=3,)
     usrname_entry.place(x=400, y=200)
     passwd = Label(window,text='Password : ')
     passwd.config(font = ('Arial', 20))
     passwd.place(x=300,y=230)
-    global passwd_entry
     passwd_entry = Entry(window, show='*', width=20, borderwidth=3,)
     passwd_entry.place(x=400, y=230)
     subission = Button(window, text='Submit', command=submit_login).place(x=390,y=260)
     forgot_pswrd = Button(window,text='forgot password',command=forgot_password).place(x=390,y=290)
     return
 def signup():
-    global window_1
+    global window_1,emaail_entry,name_entry,passwd1_entry,usrname1_entry
     window_1 = Toplevel()
     window_1.geometry('850x500')
     email = Label(window_1,text='Email Address : ')
     email.config(font = ('Arial', 20))
     email.place(x=255,y=170)
-    global emaail_entry
     emaail_entry = Entry(window_1, width=20, borderwidth=3,)
     emaail_entry.place(x=400, y=170)
     name = Label(window_1,text='Full Name : ')
     name.config(font = ('Arial', 20))
     name.place(x=292,y=200)
-    global name_entry
     name_entry = Entry(window_1, width=20, borderwidth=3,)
     name_entry.place(x=400, y=200)
     paswd = Label(window_1, text= 'Create Password : ')
     paswd.config(font = ('Arial', 20))
     paswd.place(x=230,y=230)
-    global passwd1_entry
     passwd1_entry = Entry(window_1, show='*', width=20, borderwidth=3,)
     passwd1_entry.place(x=400, y=230)
     usrnmae_1 = Label(window_1,text='Create Username : ')
     usrnmae_1.config(font = ('Arial', 20))
     usrnmae_1.place(x=226,y=260)
-    global usrname1_entry
     usrname1_entry = Entry(window_1, width=20, borderwidth=3,)
     usrname1_entry.place(x=400, y=260)
     subission_1 = Button(window_1, text='Submit', command=submit_signup).place(x=390,y=290)
     return
 def forgot_password():
-    global window_44
+    global window_44,email_entry,usr2_entry
     window_44 = Toplevel()
     window_44.geometry('850x500')
     email = Label(window_44,text='Enter email address : ')
     email.config(font = ('Arial', 20))
     email.place(x=205,y=200)
-    global email_entry
     email_entry = Entry(window_44, width=20, borderwidth=3,)
     email_entry.place(x=400, y=200)
     usr_2 = Label(window_44,text='Enter Username : ')
     usr_2.config(font = ('Arial', 20))
     usr_2.place(x=236,y=230)
-    global usr2_entry
     usr2_entry = Entry(window_44, width=20, borderwidth=3,)
     usr2_entry.place(x=400, y=230)
     subission_4 = Button(window_44, text='Submit', command=forgot_pssword).place(x=390,y=290)
